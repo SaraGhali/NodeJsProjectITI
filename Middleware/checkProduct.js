6c8185685f29735dc90ab31eeac4606cd5b6ce31
elaf <elaf6369@gmail.com>
2026-03-30 07:02:01 +0200

    Add middleware for product and cart retrieval

---
+++ Middleware/checkProduct.js
@@ -0,0 +1,12 @@
+import { productModel } from "../Database/Models/product.model.js";
+
+const checkProduct = async(req,res,next)=>{
+    let product = await productModel.findById(req.body.productId);
+    if(!product){
+        return res.status(404).json({message:"product not found"})
+    }
+    req.product = product;
+    next();
+}
+
+export default checkProduct;
