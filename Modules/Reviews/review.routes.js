1a943c981a77ef7c033d4ff3b95e0e3771f06d5e
Sara Ghali <45029076+SaraGhali@users.noreply.github.com>
2026-04-02 13:48:47 +0200

    allow only users to add/update/delete reviews

--- Modules/Reviews/review.routes.js
+++ Modules/Reviews/review.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { addReview, deleteReview, getProductReviews, updateReview } from "./review.controller.js";
 import { verifyToken } from "../../Middleware/verifyToken.js";
+import { allowedTo } from "../../Middleware/allowedTo.js";
 
 const reviewRoutes = express.Router();
 
-reviewRoutes.post("/review", verifyToken, addReview);
+reviewRoutes.post("/review", verifyToken, allowedTo("user"), addReview);
 reviewRoutes.get("/review/product/:productId", getProductReviews);
-reviewRoutes.put("/review/:id", verifyToken, updateReview);
-reviewRoutes.delete("/review/:id", verifyToken, deleteReview);
+reviewRoutes.put("/review/:id", verifyToken, allowedTo("user"), updateReview);
+reviewRoutes.delete("/review/:id", verifyToken, allowedTo("user"), deleteReview);
 
 export default reviewRoutes;
